10532fd64c9cdf8446e37b82e88eafa8e01f89bf
Panor <Panor@abc.com>
2018-12-12 14:41:15 +0700

    Replace Fragment Success

--- app/src/main/java/com/study/panor/mypanor2/MainFragment.java
+++ app/src/main/java/com/study/panor/mypanor2/MainFragment.java
@@ -2,10 +2,12 @@ package com.study.panor.mypanor2;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -20,10 +22,39 @@ public class MainFragment extends Fragment {
 
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+ //      Register Controller
+        registerController();
+
+
+    }// Mian Method
+
+    private void registerController() {
+        TextView textview = getView().findViewById(R.id.txtRegister);
+        textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+ //            Replce Fragment
+             getActivity()
+                     .getSupportFragmentManager()
+                     .beginTransaction()
+                     .replace(R.id.contentMainFragment,new RegisterFrament())
+                     .addToBackStack(null).commit();
+
+
+
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+} // Mian Class
