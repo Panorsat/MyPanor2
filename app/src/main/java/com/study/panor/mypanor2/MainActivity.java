c39746a5075c9e1e827d6452135becb8f0dba06e
Panor <Panor@abc.com>
2018-12-12 14:03:46 +0700

    Initial commit

---
+++ app/src/main/java/com/study/panor/mypanor2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.study.panor.mypanor2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //   Add Fragment
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment,new MainFragment()).commit();
+
+        }
+
+
+
+
+
+    } // Main method  จุดิ่มต้
+
+} // Main Class
