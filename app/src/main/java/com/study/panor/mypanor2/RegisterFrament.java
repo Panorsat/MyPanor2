4549aa0536bca8ae7536f53f82c94cf7b1d4a975
Panor <Panor@abc.com>
2018-12-13 10:47:42 +0700

    Alert Dialog Success

--- app/src/main/java/com/study/panor/mypanor2/RegisterFrament.java
+++ app/src/main/java/com/study/panor/mypanor2/RegisterFrament.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -29,13 +33,62 @@ public class RegisterFrament extends Fragment {
         createToolbar();
 
 
-
     } // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if (item.getItemId() == R.id.itemUpload) {
+            checkValueAndUpload();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void checkValueAndUpload() {
+
+        //   Get Value From EditText To String
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtEmail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPasword);
+        String nameString = nameEditText.getText().toString().trim();
+        String emailString = emailEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+
+     //   check space
+
+        if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty()) {
+     //      Have Space
+
+            myAlert.normalDialog("Have Space", "Please Fill Every");
+
+        } else {
+     // No Space
+
+        } // of
+
+
+
+
+        // check
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
 
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space));
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
@@ -43,12 +96,13 @@ public class RegisterFrament extends Fragment {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              getActivity().getSupportFragmentManager().popBackStack();
+
+                getActivity().getSupportFragmentManager().popBackStack();
 
             }
         });
 
-
+        setHasOptionsMenu(true);
 
 
     }
